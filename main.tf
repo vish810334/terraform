c249c69dd485cce76cdf3084466f36ea548c9061
xyz <xyz@gmail.com>
2025-07-01 00:17:13 +0530

    variable rg

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "firstrg" {
+    for_each = toset(var.myfirstrg)
+  name     = each.value
+  location = "west us"
+}
+
+variable "myfirstrg" {
+  default = ["rg1" ,"rg2","rg3"]
+ description = "mera pahela rg"
+}
+
+variable "location" {
+  default = "west us"
+}
---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.34.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
