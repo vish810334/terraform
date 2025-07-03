d4c3b4ca53bfd14f9464092ab50696b7f1b22744
xyz <xyz@gmail.com>
2025-07-03 21:44:48 +0530

    creating 3rg via list

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "firstrg" {
-    for_each = toset(var.myfirstrg)
+  for_each = toset(var.myfirstrg)
   name     = each.value
   location = "west us"
 }
 
 variable "myfirstrg" {
-  default = ["rg1" ,"rg2","rg3"]
- description = "mera pahela rg"
+  default     = ["rg1", "rg2", "rg3"]
+  description = "mera pahela rg"
 }
 
 variable "location" {
--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.34.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {
-    
-  }
+  features {}
+  subscription_id = "3167615c-646e-46e4-a729-28cf1b83629d"
+  tenant_id       = "848a4408-e401-45d6-b9ac-a2d44f891fb2"
 }
